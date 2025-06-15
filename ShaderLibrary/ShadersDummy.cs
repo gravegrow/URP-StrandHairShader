3ae407b064476e189c38cdb6edb0ab82e7f82e0a
gravegrow <gravegrow@gmail.com>
2025-06-15 18:53:07 +0300

    init

--- ShaderLibrary/ShadersDummy.cs
+++
@@ -1,3 +0,0 @@
-// HairFX Shaders folder need an Assembly Definition files to create a .cs project
-// This is a dummy shader file so the Shaders assembly is not empty.
-class HairFXShadersDummy { }
